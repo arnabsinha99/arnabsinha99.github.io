94578a1ee564b9ba1b74ccf110cf72a2df2c040b
Arnab Sinha <sarnab433@gmail.com>
2019-01-01 19:39:41 +0530

    Create Main_JS

---
+++ Main_JS.js
@@ -0,0 +1,25 @@
+//Intro
+
+alert("Hey! Welcome to my page!!");
+var a = prompt("How are you today?");
+alert("Happy to know that you are " + a);
+
+console.log("Conversation completed! Adios!");
+
+var x = 1;
+
+/* loop to print even numbers only from 1 to 10
+
+while(x<=10)
+{
+    if(x%2===0)
+    {
+        console.log("The number is " + x);
+    }
+    else{
+        x = x+1;
+        continue;
+    }
+    x = x+1;
+}*/
+
