dfa2d62524e8c0dae57d8a4d42d3dc780a74d9c9
Arnab Sinha <sarnab433@gmail.com>
2019-01-03 00:19:53 +0530

    Update Main_JS.js
    No event parameter needed

--- Main_JS.js
+++ Main_JS.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
     //Intro
   
     var header = document.getElementById("theheader");
