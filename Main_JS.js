f541f9ef8a0b352626274a3f089979a08b2bc661
Arnab Sinha <sarnab433@gmail.com>
2019-01-02 15:36:52 +0530

    Funky header

--- Main_JS.js
+++ Main_JS.js
@@ -1,25 +1,21 @@
-//Intro
-
-alert("Hey! Welcome to my page!!");
-var a = prompt("How are you today?");
-alert("Happy to know that you are " + a);
-
-console.log("Conversation completed! Adios!");
-
-var x = 1;
-
-/* loop to print even numbers only from 1 to 10
-
-while(x<=10)
-{
-    if(x%2===0)
-    {
-        console.log("The number is " + x);
+document.addEventListener("DOMContentLoaded", function (event) {
+    //Intro
+  
+    var header = document.getElementById("theheader");
+    
+    header.style.color = "red";
+    function getRandomColor() {
+        var letters = "0123456789ABCDEF";
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
-    else{
-        x = x+1;
-        continue;
+    function changeHeaderColor() {
+        colorInput = getRandomColor()
+        header.style.color = colorInput;
     }
-    x = x+1;
-}*/
 
+    setInterval(changeHeaderColor, 500);
+});
