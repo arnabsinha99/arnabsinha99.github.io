c70c91458e8d2a80af7b418b21a3334a7e777fa0
Arnab Sinha <sarnab433@gmail.com>
2019-01-02 15:37:23 +0530

    Funky header in Blogs

---
+++ My_blogs_JS.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    //Intro
+
+    var header = document.getElementById("theheader");
+
+    header.style.color = "red";
+    function getRandomColor() {
+        var letters = "0123456789ABCDEF";
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+    function changeHeaderColor() {
+        colorInput = getRandomColor()
+        header.style.color = colorInput;
+    }
+
+    setInterval(changeHeaderColor, 500);
+});
