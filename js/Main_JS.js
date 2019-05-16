623f1705fede498473c867adc0033a1da1973071
Arnab Sinha <sarnab433@gmail.com>
2019-05-16 17:20:53 +0530

    Changes
    Bootstrap CSS and JS source added.
    Changed the format of Home Page.

--- js/Main_JS.js
+++ js/Main_JS.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    //Intro
+// document.addEventListener("DOMContentLoaded", function () {
+//     //Intro
   
-    var header = document.getElementById("theheader");
+//     var header = document.getElementById("theheader");
     
-    header.style.color = "red";
-    function getRandomColor() {
-        var letters = "0123456789ABCDEF";
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-    function changeHeaderColor() {
-        colorInput = getRandomColor()
-        header.style.color = colorInput;
-    }
+//     header.style.color = "red";
+//     function getRandomColor() {
+//         var letters = "0123456789ABCDEF";
+//         var color = '#';
+//         for (var i = 0; i < 6; i++) {
+//             color += letters[Math.floor(Math.random() * 16)];
+//         }
+//         return color;
+//     }
+//     function changeHeaderColor() {
+//         colorInput = getRandomColor()
+//         header.style.color = colorInput;
+//     }
 
-    setInterval(changeHeaderColor, 500);
-});
+//     setInterval(changeHeaderColor, 500);
+// });
